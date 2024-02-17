f1949e3d41d7c5264e28f66f828ad2d573d950f5
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-02-17 17:24:36 +0500

    Hired Worker Expenses CRUD has been finished right now

--- app/Http/Resources/ShowHiredWorkerResource.php
+++ app/Http/Resources/ShowHiredWorkerResource.php
@@ -15,6 +15,7 @@ class ShowHiredWorkerResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name ,
             'phone_number' => $this->phone_number ,
             'comment' => $this->comment ,

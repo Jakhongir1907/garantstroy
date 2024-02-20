fa6d28ad5055a6a2d6def3826df58f68418dca95
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-02-20 20:33:54 +0500

    New commit

--- app/Http/Resources/ShowContractFloorResource.php
+++ app/Http/Resources/ShowContractFloorResource.php
@@ -15,6 +15,7 @@ class ShowContractFloorResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id ,
             'contract_id' => $this->contract_id ,
             'contract_name' => ($this->contract) ? $this->contract->block : '',
             'price' => $this->price ,

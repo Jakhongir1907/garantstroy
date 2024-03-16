fc6370a80b1cb0566031dd04428dbaa1d365ec37
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-03-16 12:36:57 +0500

    Dashboard

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Expense;
+use App\Models\Income;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -15,9 +18,41 @@ class DashboardController extends Controller
             $formattedDate = $currentDate->format('F Y');
             $months[] = $formattedDate;
         }
+        $currentDate = now();
 
+        // Calculate the start date (6 months ago)
+        $startDate = $currentDate->copy()->subMonths(5)->startOfMonth();
+
+        // Calculate the end date (current month)
+        $endDate = $currentDate->endOfMonth();
+
+        // Query to get total incomes for each month
+        $incomes = Income::select(DB::raw('YEAR(date) as year, MONTH(date) as month, SUM(amount) as total_income'))
+            ->whereBetween('date', [$startDate, $endDate])
+            ->groupBy(DB::raw('YEAR(date), MONTH(date)'))
+            ->get();
+
+        // Query to get total expenses for each month
+        $expenses = Expense::select(DB::raw('YEAR(date) as year, MONTH(date) as month, SUM(amount) as total_expense'))
+            ->whereBetween('date', [$startDate, $endDate])
+            ->groupBy(DB::raw('YEAR(date), MONTH(date)'))
+            ->get();
+
+        // Calculate profits for each month
+        $profits = [];
+        foreach ($incomes as $income) {
+            $profits[$income->year][$income->month] = $income->total_income;
+        }
+
+        foreach ($expenses as $expense) {
+            if (!isset($profits[$expense->year][$expense->month])) {
+                $profits[$expense->year][$expense->month] = 0;
+            }
+            $profits[$expense->year][$expense->month] -= $expense->total_expense;
+        }
         return response()->json([
-            'months' => $months
+            'months' => $months ,
+            'profits' => $profits
         ]);
     }
 }
